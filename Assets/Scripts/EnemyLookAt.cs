beb1ee3902aae84715f92daffd425e445fcec989
Sabelo Nkwanyana <2357577@students.wits.ac.za>
2023-05-09 16:49:34 +0200

    Player Move and Enemy Look

---
+++ Assets/Scripts/EnemyLookAt.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLookAt : MonoBehaviour
+{
+
+    public GameObject playerObject;
+    public float lookSpeed = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Direction = playerObject.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(Direction);
+        Quaternion lookAt = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * lookSpeed);
+        transform.rotation = lookAt;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+    public float rotationSpeed;
+
+    CharacterController controller;
+
+
+
+    [SerializeField]
+     float playerSpeed = 6.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+    private void Awake()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+}
