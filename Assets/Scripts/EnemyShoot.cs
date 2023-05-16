a344a5a83cd121b2db194034bfc680c2273f9eba
michaela-dupreez <2321949@students.wits.ac.za>
2023-05-16 11:36:11 +0200

    EnemyShootOne

---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+
+    [SerializeField] private float timer = 5;
+    private float bulletTime;
+
+    public GameObject enemyBullet;
+    public Transform spawnPoint;
+    public float enemySpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ShootAtPlayer();
+    }
+
+    void ShootAtPlayer()
+    {
+        bulletTime -= Time.deltaTime;
+        if (bulletTime > 0) return;
+
+        bulletTime = timer;
+
+        GameObject bulletObj = Instantiate(enemyBullet, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+        Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+        bulletRig.AddForce(bulletRig.transform.forward * enemySpeed);
+        Destroy(bulletObj, 5f);
+    }
+
+}
