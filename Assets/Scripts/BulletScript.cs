9e15b0e381fa7aa2e97a37cd33516dbf14b796dd
michaela-dupreez <2321949@students.wits.ac.za>
2023-05-18 14:55:21 +0200

    Damage

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public float PlayerHealth = 5;
+
+    public void Update()
+    {
+        if (PlayerHealth == 0)
+        {
+            Debug.Log("Dead");
+        }
+
+        Debug.Log(PlayerHealth);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            PlayerHealth -= 1;
             Debug.Log("Reduce Player Health");
-            Destroy(this.gameObject);
-           
+            Destroy(this.gameObject);           
         }
         else
         {
           
         }
     }
+
 }
